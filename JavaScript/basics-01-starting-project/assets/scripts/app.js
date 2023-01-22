1ff8a985fbc5b9727aa551ad1abac454b2294aa1
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2023-01-22 23:26:29 +0530

    concatination of strings

--- JavaScript/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,6 @@
-alert("This is a test")
+let currentResult = 0;
+
+currentResult = (currentResult + 10)*3/2 -1;
+calculationDisplay = '(' + currentResult + ' + 10) * 3 / 2 - 1'; //concatination of strings
+
+outputResult(currentResult, calculationDisplay);
